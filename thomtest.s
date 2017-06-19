744a8a3dd023b7c597b8ad46239aef8bdd61ff7b
Thom <tallen.thom@gmail.com>
2017-06-19 22:59:21 +0000

    Started working on getting into 'raw' mode

--- thomtest.s
+++ thomtest.s
@@ -1,44 +1,41 @@
- @ Start writing code
-@	.include "cursor.s"
-@	.include "term.s"
-	.include "mov32.inc"
+@ Start writing code
+.include "mov32.inc"
 
-	.set STDOUT, 1
-	
-	.set READ,  0x03
-	.set WRITE, 0x04
-	
-	
-	@constant min bound
-	.set MIN, 0
-	
-	@constant max bound
-	.set MAX, 60
-	
-	.balign 4
-	.data
+.set STDOUT, 1
+
+.set READ,  0x03
+.set WRITE, 0x04
+
+@constant min bound
+.set MIN, 0
+
+@constant max bound
+.set MAX, 60
+
+.balign 4
+.data
 playerBody:
 	.asciz "#"
 	.set body_len, .-playerBody
 space:
 	.ascii " "
-	
+
 gameKey:
 	.skip 4
-	
+
 player:
 	.skip 1
 	.set length, .-player
 	.word 30
 	.set player_len, .-length
-	
+
 	.balign 4
 	.text
 
 //drawGame:
 //	mov r0, #MIN
-	
-	
+
+
 drawPlayer:
 	mov r3, lr
 	mov r0, #STDOUT
@@ -49,8 +46,8 @@ drawPlayer:
 
 	mov lr, r3
 	bx lr
-	
-	
+
+
 
 gameLoop:
 	mov r4, lr
@@ -58,7 +55,7 @@ gameLoop:
 	bl clear_screen
 	bl cursor_home
 
-	
+
 /*.L1:
 	bl clear_screen
         bl cursor_home
@@ -72,58 +69,74 @@ gameLoop:
 	mov r2, #4096
 	mov r7, #READ
 	svc #0
-*/	
-	
+*/
 	mov lr, r4
 	bx lr
-	
-	
+
+
 	.global _start
 _start:
 	bl gameLoop
 	bl term_inited
-/*
-	push {r4-r12, lr}
-	sub sp, sp, #60
 
-        // ioctl(unsigned int fd, unsigned int cmd, void* arg)  r7 = 0x36
-	ldr r2, =termios // Pointer to the global struct
-	ldr r1, =TCGETS // Select ioctl function
-	mov r0, #STDIN  // Set device file handle
-	mov r7, #IOCTL
-	svc #0
-	
-        // Copy the global struct to local stack for modification
-	mov r1, sp
-	ldr r0, =termios
-	ldm r0!, {r2-r11} // Copy 40 bytes
-	stm r1!, {r2-r11}
-	ldm r0, {r2-r6} // Copy 20 bytes
-	stm r1, {r2-r6}
-
-	// Now we need to modify the struct on the stack
-	// Disable Echo and Canonical mode
-	ldr r0, [sp, #LFLAG]  // Get the control flags
-	bic r0, r0, #(ECHO | ICANON)
-	str r0, [sp, #LFLAG]
-	// Make input non-blocking, with no timeout (essentially set to polling mode)
-	mov r0, #0
-	strh r0, [sp, #(C_CC + VTIME)]  // VTIME and VMIN are adjacent bytes, so writing a halfword to VTIME should overwrite both
-
-	// Ready to write back to the device driver now
-	mov r2, sp
-	ldr r1, =TCSETS
-	mov r0, #STDIN
-	mov r7, #IOCTL
-	svc #0
-	
-	
-	add sp, sp, #60
-	pop {r4-r12, pc}
-*/	
 	sub sp, sp, #1
 	
+while_loop:
+	mov r7, #READ
+	mov r0, #STDIN
+	mov r1, sp
+	mov r2, #1
+	svc #0
+	
+        // If nothing was read, don't bother writing
+	cmp r0, #0
+	beq skip_print
+
+skip_print:
+	ldrb r0, [sp]
+	cmp r0, #32
+	bne while_loop
+
+	add sp, sp, #1
+	bl term_quit
+
 	mov r7, #1
 	svc #0
 
-	
+
+	// Print a byte as a number
+p_byte:
+	mov r3, sp // String pointer
+	sub sp, sp, #4
+
+	mov r12, #10
+
+	mov r1, #10     // Newline
+	strb r1, [r3, #-1]!
+
+
+	b test_non_zero
+while_digits:
+	// Divide/Modulus (r1, r2)
+	udiv r1, r0, r12
+	mls r2, r1, r12, r0
+
+	add r2, r2, #48  // r2 = r2 + "0"
+	strb r2, [r3, #-1]!
+
+	mov r0, r1
+
+test_non_zero:
+	cmp r0, #0
+	bne while_digits
+
+
+	mov r7, #WRITE
+	mov r0, #STDOUT
+	mov r1, r3
+	add r2, sp, #4
+	sub r2, r2, r1
+	svc #0
+
+	add sp, sp, #4
+	bx lr
