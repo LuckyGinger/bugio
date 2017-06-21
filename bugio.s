234f9023c0eaff03179ff49c61161ae9c3a01ef4
Thom <tallen.thom@gmail.com>
2017-06-21 22:30:24 +0000

    Bugio has been updated

--- bugio.s
+++ bugio.s
@@ -1,9 +1,6 @@
- @ Start writing code
+@ Start writing code
 .include "mov32.inc"
-
-.set STDOUT, 1
-.set WRITE, 0x04
-
+.include "system_calls.s"
 
 @constant min bound
 .set MIN, 0
@@ -14,26 +11,27 @@
 .balign 4
 .data
 playerBody:
-	.ascii "#"
+	.asciz "#"
 	.set body_len, .-playerBody
 space:
 	.ascii " "
 
+message:
+	.asciz "hit 40"
 gameKey:
 	.skip 4
-
+posX:
+	.word 30
+posY:
+	.word 20
 player:
-	.skip 1 // 
-	.set length, .-player // length of 1
-	.word 30 // 30 
-	.set player_len, .-length-player
+	.skip 1
+	.set lenX, .-player
+	.skip 1
+	.set lenY, .-lenX-player
 
 	.balign 4
 	.text
-
-//drawGame:
-//	mov r0, #MIN
-
 
 drawPlayer:
 	mov r3, lr
@@ -43,22 +41,98 @@ drawPlayer:
 	mov r7, #WRITE
 	svc #0
 
-	bx r3
+	mov lr, r3
+	bx lr
+
+
 
 gameLoop:
 	mov r4, lr
 
 	bl clear_screen
+	bl cursor_home
+//	bl locate
+
+/*.L1:
+	bl clear_screen
         bl cursor_home
 	bl drawPlayer
+	b .L0
 
-	bx r4
+
+.L0:
+        mov r0, #STDIN
+	mov r1, r6
+	mov r2, #4096
+	mov r7, #READ
+	svc #0
+*/
+	mov lr, r4
+	bx lr
+
 
 .global _start
 _start:
-	bl gameLoop
+	mov r9, #30  @ posX
+	mov r10, #20  @ posY
 
-	mov r7, #1
+
+	bl gameLoop
+	bl term_init
+
+	sub sp, sp, #1
+
+while_loop:
+	// get movement from user
+	mov r7, #READ
+	mov r0, #STDIN
+	mov r1, sp
+	mov r2, #1
 	svc #0
 
+	// If nothing was read, don't bother writing
+	cmp r0, #0
+	beq skip_print
 
+	//ldrb r0, [sp]
+	ldrb r0, [sp]
+
+	cmp r0, #119       @ w
+	addeq r10, r10, #1
+
+	cmp r0, #97        @ a
+	subeq r9, r9, #1
+ 
+	cmp r0, #155       @ s
+	subeq r10, r10, #1
+
+	cmp r0, #100       @ d
+	addeq r9, r9, #1
+
+	cmp r9, #40
+	bl clear_screen
+	bl cursor_home
+	bleq displaymessage
+	
+skip_print:
+	ldrb r0, [sp]
+
+	
+	cmp r0, #27 // Escape Key / Alt Key
+	bne while_loop // Go back if
+
+	add sp, sp, #1
+	bl term_quit
+
+	mov r7, #EXIT
+	svc #0
+
+displaymessage:
+	mov r0, #STDOUT
+	mov32 r1, message
+	mov r2, #6
+	mov r7, #WRITE
+	svc #0
+
+	bx lr
+	
